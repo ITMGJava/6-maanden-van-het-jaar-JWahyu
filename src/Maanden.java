6d175d68f79638ebc1a53cd00e0ad9077036b0c7
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-10-03 02:08:46 +0200

    Switch module gereviseerd met toevoeging van Do-While loop functie. Output layout aanpassingen.
    Nieuwe Switch Module gevonden.

--- src/Maanden.java
+++ src/Maanden.java
@@ -18,44 +18,19 @@ public class Maanden {
 
             // Selectie maand d.m.v. switch.
             switch (month) {
-                case 1:
-                    System.out.printf("Maand 1: Net het nieuwe jaar ingedoken met januari!%n");
-                    break;
-                case 2:
-                    System.out.printf("Maand 2: Februari, de kortste maand van het jaar.%n");
-                    break;
-                case 3:
-                    System.out.printf("Maand 3: Een droge maart, is een zomer te paard.%n");
-                    break;
-                case 4:
-                    System.out.printf("Maand 4: April doet wat hij wil.%n");
-                    break;
-                case 5:
-                    System.out.printf("Maand 5: Het is zo stil in mei.%n");
-                    break;
-                case 6:
-                    System.out.printf("Maand 6: Zomerse dagen in juni.%n");
-                    break;
-                case 7:
-                    System.out.printf("Maand 7: Heerlijk op vakantie in juli!%n");
-                    break;
-                case 8:
-                    System.out.printf("Maand 8: Geniet van het laatste warme weer in augustus.%n");
-                    break;
-                case 9:
-                    System.out.printf("Maand 9: De scholen beginnen weer in september.%n");
-                    break;
-                case 10:
-                    System.out.printf("Maand 10: Halloween en guur weer in oktober.%n");
-                    break;
-                case 11:
-                    System.out.printf("Maand 11: Misschien valt er wel sneeuw in november.%n");
-                    break;
-                case 12:
-                    System.out.printf("Maand 12: Sinterklaas, kerst en het einde van het jaar in december!%n");
-                    break;
-                default:
-                    System.out.printf("Incorrecte invoer. Vul a.u.b. een getal tussen 1 en 12:%n");
+                case 1 -> System.out.printf("Maand 1: Net het nieuwe jaar ingedoken met januari!%n");
+                case 2 -> System.out.printf("Maand 2: Februari, de kortste maand van het jaar.%n");
+                case 3 -> System.out.printf("Maand 3: Een droge maart, is een zomer te paard.%n");
+                case 4 -> System.out.printf("Maand 4: April doet wat hij wil.%n");
+                case 5 -> System.out.printf("Maand 5: Het is zo stil in mei.%n");
+                case 6 -> System.out.printf("Maand 6: Zomerse dagen in juni.%n");
+                case 7 -> System.out.printf("Maand 7: Heerlijk op vakantie in juli!%n");
+                case 8 -> System.out.printf("Maand 8: Geniet van het laatste warme weer in augustus.%n");
+                case 9 -> System.out.printf("Maand 9: De scholen beginnen weer in september.%n");
+                case 10 -> System.out.printf("Maand 10: Halloween en guur weer in oktober.%n");
+                case 11 -> System.out.printf("Maand 11: Misschien valt er wel sneeuw in november.%n");
+                case 12 -> System.out.printf("Maand 12: Sinterklaas, kerst en het einde van het jaar in december!%n");
+                default -> System.out.printf("Incorrecte invoer. Vul a.u.b. een getal tussen 1 en 12:%n");
             }
         } while (!(month >= 1 && month <= 12));
 // Einde opdracht.
